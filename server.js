3c36c9e92c93b16d1f0466a9be99d40c9a34812e
JahongirHamidov <hamidovjahongir444@gmail.com>
2021-01-03 18:18:18 +0500

    client and sever connected

--- server.js
+++ server.js
@@ -14,6 +14,10 @@ const transactions = require('./routes/transactions.js')
 
 app.use(express.json())
 
+if(process.env.NODE_ENV === 'development'){
+    app.use(morgan('dev'))
+}
+
 app.use('/api/v1/transactions', transactions)
 
 const PORT = process.env.PORT || 5000
